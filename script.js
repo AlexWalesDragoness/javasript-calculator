2a7bdc77ef6749b77f70279214fbf8465a4a2f4e
AlexWalesDragoness <110855001+AlexWalesDragoness@users.noreply.github.com>
2024-07-30 00:00:44 +0100

    Added files
    Added html, css, and js files to github repository

---
+++ script.js
@@ -0,0 +1,33 @@
+//get each element by the id or class name
+
+const number = document.querySelectorAll(".number");
+const output = document.getElementsByClassName(".output");
+const clearbutton = document.getElementById("#clearbutton");
+const equalsbutton = document.getElementById("#equalsbutton");
+const addbutton = document.getElementById("#addbutton");
+const dividebutton = document.getElementById("#dividebutton");
+const multiplybutton = document.getElementById("#multiplybutton");
+const subtractbutton = document.getElementById("#subtractbutton");
+const numbers = [];
+
+/* add a function to execute when the button is clicked. ? how to differentiate between the buttons and make this function work for every button */
+document.getElementById("number").addEventListener("click, displayResult");
+
+function displayResult() {
+  //make this display in the output bar
+}
+
+// Then repeat for the operator and then the next number etc
+// Then display final result
+
+// I'm so confused ! I've seen these codes as examples but they make no sense to me.
+const display_input = document.querySelector(".display_input");
+const display_output = document.querySelector(".display_output");
+
+let input = "";
+
+for (let number of numbers) {
+  const value = parent.child.number.value;
+}
+
+//Yep, I have definitely made this too complicated....
